757ac27b5a32088f42e3eab7b2f34e3067769b31
Yamashchita Rikaenzhela <rikitiki21@gmail.com>
2025-11-12 19:36:13 +0200

    Lb10: Final

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import Header from "./components/Header.jsx";
 import Sidenav from "./components/Sidenav.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="layout">
-      <Header />
-      <Sidenav />
-      <main className="main">
-        <Home />
-      </main>
-      <Footer />
-    </div>
-  )
+    const [page, setPage] = useState('home');
+
+    return (
+        <div className="layout">
+            <Header onNavigate={setPage} />
+            <Sidenav/>
+            <main className="main">
+                {page === "home" && <Home/>}
+                {page === "about" && <About/>}
+            </main>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
